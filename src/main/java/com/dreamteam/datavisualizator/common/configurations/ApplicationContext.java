96b01ac52f7feb5f0a75520d70a48eb453f8d0a3
Boris Khripko <boris.katran@gmail.com>
2017-05-12 13:42:34 +0300

    NCSPRIN-28 preliminary creation of dataSource and txManager

--- src/main/java/com/dreamteam/datavisualizator/common/configurations/ApplicationContext.java
+++ src/main/java/com/dreamteam/datavisualizator/common/configurations/ApplicationContext.java
@@ -1,27 +1,28 @@
 
 package com.dreamteam.datavisualizator.common.configurations;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan(basePackages = "com.dreamteam.datavisualizator")
-
 public class ApplicationContext {
 
     @Bean(name = "dataSource")
     public DataSource getDataSource() {
-        return null;
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(System.getenv(""));
+        dataSource.setUrl(System.getenv(""));
+        dataSource.setUsername(System.getenv(""));
+        dataSource.setPassword(System.getenv(""));
+        return dataSource;
     }
 
-    @Autowired
     @Bean(name = "transactionManager")
     public DataSourceTransactionManager getTransactionManager(DataSource dataSource) {
-        return null;
+        return new DataSourceTransactionManager(dataSource);
     }
 }
