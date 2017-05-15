006cf06e34967f552263706a66ceb029dcfd51f4
Sergey Pavlychenko <seraphicps@mail.ru>
2017-05-15 16:01:43 +0300

    NCSPRIN-40 Fix ControllerDev

--- src/main/java/com/dreamteam/datavisualizator/controllers/ProjectController.java
+++ src/main/java/com/dreamteam/datavisualizator/controllers/ProjectController.java
@@ -1,13 +1,25 @@
 package com.dreamteam.datavisualizator.controllers;
 
 import com.dreamteam.datavisualizator.dao.DataVisualizationProjectDAO;
-import com.dreamteam.datavisualizator.dao.impl.UserDAOImpl;
+import com.dreamteam.datavisualizator.dao.UserDAO;
+import com.dreamteam.datavisualizator.models.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.math.BigInteger;
 
 //@Controller
-public class ProjectController extends UserDAOImpl  {
+public class ProjectController {
     @Autowired
     DataVisualizationProjectDAO projectdao;
+
+    @RequestMapping(path = "/deleteProject", method = RequestMethod.GET)
+    public void deleteProject(Project project,
+                              Model model) {
+        projectdao.deleteProject(project);
+    }
 
 }
--- src/main/java/com/dreamteam/datavisualizator/controllers/UserController.java
+++ src/main/java/com/dreamteam/datavisualizator/controllers/UserController.java
@@ -1,14 +1,24 @@
 package com.dreamteam.datavisualizator.controllers;
 
 
+import com.dreamteam.datavisualizator.dao.DataVisualizationProjectDAO;
 import com.dreamteam.datavisualizator.dao.UserDAO;
-import com.dreamteam.datavisualizator.dao.impl.UserDAOImpl;
+import com.dreamteam.datavisualizator.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 //@Controller
-public class UserController extends UserDAOImpl{
+public class UserController {
     @Autowired
-    UserDAO userdao;
+    private UserDAO userDAO;
+
+    @RequestMapping(path = "/deleteUser", method = RequestMethod.GET)
+    public void deleteUser(User user,
+                           Model model) {
+        userDAO.deleteUser(user);
+    }
 
 }
