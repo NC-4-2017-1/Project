1fc48d5593132c8018e62f8af1089902a9868fb2
Tiurina Veronika <ververo3@gmail.com>
2017-05-22 15:31:30 +0300

    NCSPRIN-38 - select queries for selectors

--- src/main/java/com/dreamteam/datavisualizator/dao/impl/HealthMonitorProjectDAOImpl.java
+++ src/main/java/com/dreamteam/datavisualizator/dao/impl/HealthMonitorProjectDAOImpl.java
@@ -12,13 +12,18 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
 import java.math.BigInteger;
 import java.sql.Clob;
 import java.sql.ResultSet;
@@ -90,6 +95,27 @@ public class HealthMonitorProjectDAOImpl extends AbstractDAO implements HealthMo
     public List<Selector> createSelectorList(Map<String, String> map) {
         try {
             //TODO method for saving buildSelectors
+            SimpleJdbcCall simpleCallTemplateInstanceInfo = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateSizeForTablespace = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateSizeForIndexLob = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateLastErrors = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateActiveSessions = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateActiveQueries = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateQueriesResults = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateSqlQueryMonitor = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateDBLocks = new SimpleJdbcCall(generalTemplate);
+            SimpleJdbcCall simpleCallTemplateActiveJobs = new SimpleJdbcCall(generalTemplate);
+
+            createSelectorInstanceInfo(simpleCallTemplateInstanceInfo);
+            createSelectorSizeForTablespace(simpleCallTemplateSizeForTablespace);
+            createSelectorSizeForIndexLob(simpleCallTemplateSizeForIndexLob, "dba_extents");
+            createSelectorLastErrors(simpleCallTemplateLastErrors);
+            createSelectorActiveSessions(simpleCallTemplateActiveSessions, 10);
+            createSelectorActiveQueries(simpleCallTemplateActiveQueries, 10);
+            createSelectorQueriesResults(simpleCallTemplateQueriesResults, 10);
+            createSelectorSqlQueryMonitor(simpleCallTemplateSqlQueryMonitor,10);
+            createSelectorDBLocks(simpleCallTemplateDBLocks);
+            createSelectorActiveJobs(simpleCallTemplateActiveJobs, 2);
             return null;
         } catch (Exception e) {
             LOGGER.error("List of buildSelectors not created", e);
@@ -167,59 +193,85 @@ public class HealthMonitorProjectDAOImpl extends AbstractDAO implements HealthMo
         return true;
     }
 
-    private SelectorInstanceInfo createSelectorInstanceInfo() {
+    private SelectorInstanceInfo createSelectorInstanceInfo(SimpleJdbcCall simpleCallTemplateInstanceInfo) {
         SelectorInstanceInfo selector = new SelectorInstanceInfo();
+        simpleCallTemplateInstanceInfo.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_INSTANCE_INFO);
+        String sql_query = simpleCallTemplateInstanceInfo.executeFunction(String.class).toString();
         return selector;
     }
 
-    private SelectorSizeForTablespace createSelectorSizeForTablespace() {
+    private SelectorSizeForTablespace createSelectorSizeForTablespace(SimpleJdbcCall simpleCallTemplateSizeForTablespace) {
         SelectorSizeForTablespace selector = new SelectorSizeForTablespace();
+        simpleCallTemplateSizeForTablespace.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_SIZE_TABLESPACE);
+        String sql_query = simpleCallTemplateSizeForTablespace.executeFunction(String.class).toString();
         return selector;
     }
 
-    private SelectorSizeForIndexLob createSelectorSizeForIndexLob(String segment) {
+    private SelectorSizeForIndexLob createSelectorSizeForIndexLob(SimpleJdbcCall simpleCallTemplateSizeForIndexLob, String segment) {
         SelectorSizeForIndexLob selector = new SelectorSizeForIndexLob();
         selector.setSegment(segment);
+        simpleCallTemplateSizeForIndexLob.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_SIZE_INDEX_LOB);
+        SqlParameterSource in = new MapSqlParameterSource().addValue("segment", segment);
+        String sql_query = simpleCallTemplateSizeForIndexLob.executeFunction(String.class, in).toString();
         return selector;
     }
 
-    private SelectorLastErrors createSelectorLastErrors() {
+    private SelectorLastErrors createSelectorLastErrors(SimpleJdbcCall simpleCallTemplateLastErrors) {
         SelectorLastErrors selector = new SelectorLastErrors();
+        simpleCallTemplateLastErrors.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_LAST_ERRORS);
+        String sql_query = simpleCallTemplateLastErrors.executeFunction(String.class).toString();
         return selector;
     }
 
-    private SelectorActiveSessions createSelectorActiveSessions(int top) {
+    private SelectorActiveSessions createSelectorActiveSessions(SimpleJdbcCall simpleCallTemplateActiveSessions, int top) {
         SelectorActiveSessions selector = new SelectorActiveSessions();
         selector.setTop(top);
+        simpleCallTemplateActiveSessions.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_ACTIVE_SESSIONS);
+        SqlParameterSource in = new MapSqlParameterSource().addValue("top", top);
+        String sql_query = simpleCallTemplateActiveSessions.executeFunction(String.class, in).toString();
         return selector;
     }
 
-    private SelectorActiveQueries createSelectorActiveQueries(int top) {
+    private SelectorActiveQueries createSelectorActiveQueries(SimpleJdbcCall simpleCallTemplateActiveQueries, int top) {
         SelectorActiveQueries selector = new SelectorActiveQueries();
         selector.setTop(top);
+        simpleCallTemplateActiveQueries.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_ACTIVE_QUERIES);
+        SqlParameterSource in = new MapSqlParameterSource().addValue("top", top);
+        String sql_query = simpleCallTemplateActiveQueries.executeFunction(String.class, in).toString();
         return selector;
     }
 
-    private SelectorQueriesResults createSelectorQueriesResults(int top) {
+    private SelectorQueriesResults createSelectorQueriesResults(SimpleJdbcCall simpleCallTemplateQueriesResults, int top) {
         SelectorQueriesResults selector = new SelectorQueriesResults();
         selector.setTop(top);
+        simpleCallTemplateQueriesResults.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_QUERIES_RESULTS);
+        SqlParameterSource in = new MapSqlParameterSource().addValue("top", top);
+        String sql_query = simpleCallTemplateQueriesResults.executeFunction(String.class, in).toString();
         return selector;
     }
 
-    private SelectorSqlQueryMonitor createSelectorSqlQueryMonitor(int top) {
+    private SelectorSqlQueryMonitor createSelectorSqlQueryMonitor(SimpleJdbcCall simpleCallTemplateSqlQueryMonitor, int top) {
         SelectorSqlQueryMonitor selector = new SelectorSqlQueryMonitor();
         selector.setTop(top);
+        simpleCallTemplateSqlQueryMonitor.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_SQL_MONITOR);
+        SqlParameterSource in = new MapSqlParameterSource().addValue("top", top);
+        String sql_query = simpleCallTemplateSqlQueryMonitor.executeFunction(String.class, in).toString();
         return selector;
     }
 
-    private SelectorDBLocks createSelectorDBLocks() {
+    private SelectorDBLocks createSelectorDBLocks(SimpleJdbcCall simpleCallTemplateDBLocks) {
         SelectorDBLocks selector = new SelectorDBLocks();
+        simpleCallTemplateDBLocks.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_DB_LOCKS);
+        String sql_query = simpleCallTemplateDBLocks.executeFunction(String.class).toString();
         return selector;
     }
 
-    private SelectorActiveJobs createSelectorActiveJobs(int hourCount) {
+    private SelectorActiveJobs createSelectorActiveJobs(SimpleJdbcCall simpleCallTemplateActiveJobs, int hourCount) {
         SelectorActiveJobs selector = new SelectorActiveJobs();
         selector.setHourCount(hourCount);
+        simpleCallTemplateActiveJobs.withCatalogName(SELECTOR_QUERIES_PACKAGE).withFunctionName(QUERY_FOR_ACTIVE_JOBS);
+        SqlParameterSource in = new MapSqlParameterSource().addValue("hour_count", hourCount);
+        String sql_query = simpleCallTemplateActiveJobs.executeFunction(String.class, in).toString();
         return selector;
     }
 
@@ -307,11 +359,17 @@ public class HealthMonitorProjectDAOImpl extends AbstractDAO implements HealthMo
             " AND ref.object_id = hmproject.object_id" +
             " AND ref.reference = author.object_id" +
             " AND hmproject.name = ? ";
-    private static final String SELECT_SELECTOR_INSTANCE_INFO = "select BANNER oracle_version," +
-            " sys_context('userenv', 'DB_NAME') db_name," +
-            " sys_context('userenv','CURRENT_SCHEMA') schem_name" +
-            " from v$version where rownum=1";
-
+    private static final String SELECTOR_QUERIES_PACKAGE = "pkg_selectors";
+    private static final String QUERY_FOR_INSTANCE_INFO = "instance_info";
+    private static final String QUERY_FOR_SIZE_TABLESPACE = "size_for_tablespace";
+    private static final String QUERY_FOR_SIZE_INDEX_LOB = "size_for_index_lob";
+    private static final String QUERY_FOR_LAST_ERRORS = "last_errors";
+    private static final String QUERY_FOR_ACTIVE_SESSIONS = "active_sessions";
+    private static final String QUERY_FOR_ACTIVE_QUERIES = "active_queries";
+    private static final String QUERY_FOR_QUERIES_RESULTS = "queries_results";
+    private static final String QUERY_FOR_SQL_MONITOR = "sql_query_monitor";
+    private static final String QUERY_FOR_DB_LOCKS = "db_locks";
+    private static final String QUERY_FOR_ACTIVE_JOBS = "active_jobs";
 
     private enum HWProjectColumnName {
         id("id"),
