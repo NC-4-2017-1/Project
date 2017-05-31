77c106ec5891a2f85ae50267c81fe1b9cfff0153
ShtakhovYevhen <creshibon@gmail.com>
2017-06-01 00:30:44 +0300

    NCSPRIN-43 CSV parser work? but without exception, Correct file csv Add sum date formate

--- src/main/java/com/dreamteam/datavisualizator/common/dateconverter/DateFormat.java
+++ src/main/java/com/dreamteam/datavisualizator/common/dateconverter/DateFormat.java
@@ -16,8 +16,14 @@ public enum DateFormat {
     WET_WITH_TIME_AND_HYPHEN_DELIMITER(BigInteger.valueOf(9L), "MM-dd-yy HH:mm:ss"),
     WET_WITH_HYPHEN_DELIMITER(BigInteger.valueOf(10L), "MM-dd-yy"),
     WET_WITH_TIME_AND_SLASH_DELIMITER(BigInteger.valueOf(11L), "MM/dd/yy HH:mm:ss"),
-    WET_WITH_SLASH_DELIMITER(BigInteger.valueOf(12L), "MM/dd/yy");
+    WET_WITH_SLASH_DELIMITER(BigInteger.valueOf(12L), "MM/dd/yy"),
 
+    EET_WITH_TIME_AND_DOT_DELIMITER_WITHOUT_SECONDS (BigInteger.valueOf(13L), "dd.MM.yy HH:mm"),
+    EET_WITH_TIME_AND_HYPHEN_DELIMITER_WITHOUT_SECONDS(BigInteger.valueOf(14L), "dd-MM-yy HH:mm"),
+    EET_WITH_TIME_AND_SLASH_DELIMITER_WITHOUT_SECONDS(BigInteger.valueOf(15L), "dd/MM/yy HH:mm"),
+    WET_WITH_TIME_AND_DOT_DELIMITER_WITHOUT_SECONDS(BigInteger.valueOf(16L), "MM.dd.yy HH:mm"),
+    WET_WITH_TIME_AND_HYPHEN_DELIMITER_WITHOUT_SECONDS(BigInteger.valueOf(17L), "MM-dd-yy HH:mm"),
+    WET_WITH_TIME_AND_SLASH_DELIMITER_WITHOUT_SECONDS(BigInteger.valueOf(18L), "MM/dd/yy HH:mm");
     private final BigInteger id;
     private final String dateFormat;
 
