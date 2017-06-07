762abcf039a6bbee188cb2262d6432de65e78774
Kirill Kozhedubov <veniq137@gmail.com>
2017-06-07 13:49:00 +0300

    NCSPRIN-80 deleted  dangerous code I added for tests

--- src/main/java/com/dreamteam/datavisualizator/controllers/IndexController.java
+++ src/main/java/com/dreamteam/datavisualizator/controllers/IndexController.java
@@ -2,7 +2,6 @@ package com.dreamteam.datavisualizator.controllers;
 
 import com.dreamteam.datavisualizator.dao.DataVisualizationProjectDAO;
 import com.dreamteam.datavisualizator.dao.UserDAO;
-import com.dreamteam.datavisualizator.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.sql.DataSource;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.TimeZone;
 
 @Controller
 public class IndexController {
@@ -27,8 +22,6 @@ public class IndexController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String getHello(Model model) {
-    /*   model.addAttribute( "userEMAIL", userdao.getUserByFullName("uname usurname").toString());
-       model.addAttribute("userID", userdao.getUserById(new BigInteger("1")).toString());*/
         return "index";
     }
 
@@ -37,29 +30,6 @@ public class IndexController {
         return "authorization";
     }
 
-    @RequestMapping(path = "/herokutest", method = RequestMethod.GET)
-    public String testHeroku(Model model) {
-        model.addAttribute("sysvar1", System.getenv("SQL_JDBC_URL"));
-        model.addAttribute("sysvar2", System.getenv("SQL_LOGIN"));
-        model.addAttribute("sysvar3", System.getenv("SQL_PASSWORD"));
-        model.addAttribute("datasource", dataSource);
-        model.addAttribute("userdao", userdao);
-        model.addAttribute("projectdao", projectdao);
-        try {
-            List<User> users = userdao.getAllUsersList();
-            model.addAttribute("users", users);
-        } catch (Exception e) {
-            model.addAttribute("exception", e);
-        }
-
-        Date date = new Date();
-        SimpleDateFormat sdf;
-        sdf = new SimpleDateFormat("dd MMM yyyy hh:mm:ss zzz");
-        System.out.println();
-        model.addAttribute("time", sdf.format(date));
-        model.addAttribute("timezone", TimeZone.getDefault().toString() + " " + TimeZone.getDefault().getDisplayName());
-        return "herokutest";
-    }
 
 
 }
