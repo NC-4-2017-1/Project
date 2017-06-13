14bde139e83e3be5be5389eb601d4d95d11bf733
ShtakhovYevhen <creshibon@gmail.com>
2017-06-13 23:02:56 +0300

    NCSPRIN-94 create model to Graph Project

--- src/main/java/com/dreamteam/datavisualizator/models/ProjectTypes.java
+++ src/main/java/com/dreamteam/datavisualizator/models/ProjectTypes.java
@@ -5,7 +5,8 @@ import java.math.BigInteger;
 
 public enum ProjectTypes {
     DATA_VISUALIZATION(BigInteger.valueOf(4L), "Data Visualization"),
-    HEALTH_MONITORING(BigInteger.valueOf(3L), "Health Monitoring");
+    HEALTH_MONITORING(BigInteger.valueOf(3L), "Health Monitoring"),
+    GRAPH(BigInteger.valueOf(18L),"Graph");
 
 
     private final BigInteger id;
---
+++ src/main/java/com/dreamteam/datavisualizator/models/impl/GraphProject.java
@@ -0,0 +1,166 @@
+package com.dreamteam.datavisualizator.models.impl;
+
+import com.dreamteam.datavisualizator.models.ProjectTypes;
+import com.dreamteam.datavisualizator.models.User;
+import com.google.gson.JsonObject;
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.List;
+
+public class GraphProject extends AbstractProject {
+    private String sid;
+    private String port;
+    private String serverName;
+    private String userName;
+    private String password;
+    private BigInteger idFirstElemGraph;
+    private BigInteger referenceAttributeGraph;
+    private JsonObject graphJson;
+
+
+
+    private GraphProject(GraphBuilder builder) {
+        this.setId(builder.id);
+        this.setName(builder.name);
+        this.setCreationDate(builder.creationDate);
+        this.setDescription(builder.description);
+        this.setAuthor(builder.author);
+        this.setAuthorFullName(builder.authorFullName);
+        this.setSid(builder.sid);
+        this.setPort(builder.port);
+        this.setServerName(builder.serverName);
+        this.setUserName(builder.userName);
+        this.setPassword(builder.password);
+        this.setUsersProjectAccessible(builder.usersProjectAccessible);
+        this.setIdFirstElemGraph(builder.idFirstElemGraph);
+        this.setType(ProjectTypes.GRAPH);
+        this.setGraphJson(builder.graphJson);
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public BigInteger getIdFirstElemGraph() {
+        return idFirstElemGraph;
+    }
+
+    public void setIdFirstElemGraph(BigInteger idFirstElemGraph) {
+        this.idFirstElemGraph = idFirstElemGraph;
+    }
+
+    public BigInteger getReferenceAttributeGraph() {
+        return referenceAttributeGraph;
+    }
+
+    public void setReferenceAttributeGraph(BigInteger referenceAttributeGraph) {
+        this.referenceAttributeGraph = referenceAttributeGraph;
+    }
+    public JsonObject getGraphJson() {
+        return graphJson;
+    }
+
+    public void setGraphJson(JsonObject graphJson) {
+        this.graphJson = graphJson;
+    }
+
+    public static class GraphBuilder {
+        private BigInteger id;
+        private String name;
+        private Date creationDate;
+        private String description;
+        private BigInteger author;
+        private String sid;
+        private String port;
+        private String serverName;
+        private String userName;
+        private String password;
+        private String authorFullName;
+        private List<User> usersProjectAccessible;
+        private BigInteger idFirstElemGraph;
+        private BigInteger referenceAttributeGraph;
+        private JsonObject graphJson;
+
+
+        public GraphBuilder(BigInteger id, String name, Date creationDate, String description, BigInteger author, String authorFullName){
+            this.id = id;
+            this.name = name;
+            this.creationDate = creationDate;
+            this.description = description;
+            this.author = author;
+            this.authorFullName = authorFullName;
+
+        }
+
+        public GraphBuilder(BigInteger id, String name, Date creationDate, String description, BigInteger author, String authorFullName,
+                       String sid, String port, String serverName, String userName, String password,BigInteger idFirstElemGraph,JsonObject graphJson) {
+            this.id = id;
+            this.name = name;
+            this.creationDate = creationDate;
+            this.description = description;
+            this.sid = sid;
+            this.port = port;
+            this.serverName = serverName;
+            this.userName = userName;
+            this.password = password;
+            this.author = author;
+            this.authorFullName = authorFullName;
+            this.idFirstElemGraph = idFirstElemGraph;
+            this.graphJson=graphJson;
+        }
+
+        public GraphBuilder buildreferenceAttribute(BigInteger val) {
+            referenceAttributeGraph = val;
+            return this;
+        }
+
+        public GraphBuilder buildUsersProjectAccessible(List<User> val) {
+            usersProjectAccessible = val;
+            return this;
+        }
+
+        public GraphProject buildProject(){
+            return new GraphProject(this);
+        }
+
+    }
+}
