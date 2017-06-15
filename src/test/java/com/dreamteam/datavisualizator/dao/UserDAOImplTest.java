160b923031f77fb351efffdb413b13dae1aba222
Boris Khripko <boris.katran@gmail.com>
2017-06-15 17:08:03 +0300

    NCSPRIN-61 preliminary version of UserDaoImplTest.class completed

--- src/test/java/com/dreamteam/datavisualizator/dao/UserDAOImplTest.java
+++ src/test/java/com/dreamteam/datavisualizator/dao/UserDAOImplTest.java
@@ -5,7 +5,6 @@ import com.dreamteam.datavisualizator.models.*;
 import com.dreamteam.datavisualizator.models.impl.*;
 import com.google.gson.JsonObject;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +27,6 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {ServletContext.class, UserDAOImplTest.UserDaoCustomConfiguration.class})
-@Ignore
 public class UserDAOImplTest {
 
     @Autowired
@@ -41,7 +39,7 @@ public class UserDAOImplTest {
     private HealthMonitorProjectDAO hmDao;
 
     @Configuration
-    public static class UserDaoCustomConfiguration{
+    public static class UserDaoCustomConfiguration {
         @Bean(name = "dataSource")
         public DataSource getDataSource() {
             TimeZone timeZone = TimeZone.getTimeZone("Europe/Helsinki");
@@ -60,7 +58,7 @@ public class UserDAOImplTest {
     private Project hmProject;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         List<Graphic> graphics = new ArrayList<>();
         Graphic dvGraphic = new GraphicDVImpl.DVGraphBuilder()
                 .buildGraphicJSON(new JsonObject())
@@ -90,7 +88,7 @@ public class UserDAOImplTest {
         Map<BigInteger, Selector> selectors = new HashMap<>();
         selectors.put(BigInteger.valueOf(8L), testSelector);
 
-        hmProject = new HealthMonitorProject.Builder(null,"HMTestProject", new Date(), "HMTestProjectDescription", null, null, "sid", "port", "server", "user", "password")
+        hmProject = new HealthMonitorProject.Builder(null, "HMTestProject", new Date(), "HMTestProjectDescription", null, null, "sid", "port", "server", "user", "password")
                 .buildDescription("HMTestProjectDescription")
                 .buildGraphic(hmGraphic)
                 .buildSelectors(selectors)
@@ -100,14 +98,14 @@ public class UserDAOImplTest {
     @Test
     @Transactional
     @Rollback
-    public void getUserByIdWithCorrectId(){
+    public void getUserByIdWithCorrectId() {
         User expectedUser = dao.createUser("testFirstName", "testLastName", "test.email@email.com", "testpassword", UserTypes.REGULAR_USER);
         User actualUser = dao.getUserById(expectedUser.getId());
         assertEquals(expectedUser.getId(), actualUser.getId());
     }
 
     @Test
-    public void getUserByIdWithIncorrectId(){
+    public void getUserByIdWithIncorrectId() {
         User actualUser = dao.getUserById(null);
         assertNull(actualUser);
     }
@@ -115,14 +113,14 @@ public class UserDAOImplTest {
     @Test
     @Transactional
     @Rollback
-    public void getUserByEmailWithCorrectEmail(){
+    public void getUserByEmailWithCorrectEmail() {
         User expectedUser = dao.createUser("testFirstName", "testLastName", "test.email@email.com", "testpassword", UserTypes.REGULAR_USER);
         User actualUser = dao.getUserByEmail(expectedUser.getEmail());
         assertEquals(expectedUser.getEmail(), actualUser.getEmail());
     }
 
     @Test
-    public void getUserByEmailWithIncorrectEmail(){
+    public void getUserByEmailWithIncorrectEmail() {
         User actualUser = dao.getUserByEmail(null);
         assertNull(actualUser);
     }
@@ -130,11 +128,11 @@ public class UserDAOImplTest {
     @Test
     @Transactional
     @Rollback
-    public void getAllUsersList(){
+    public void getAllUsersList() {
         User user1 = dao.createUser("testFirstName1", "testLastName1", "test.email@email.com1", "testpassword1", UserTypes.REGULAR_USER);
         User user2 = dao.createUser("testFirstName2", "testLastName2", "test.email@email.com2", "testpassword2", UserTypes.REGULAR_USER);
         List users = dao.getAllUsersList();
-        assertTrue(users.size()>=2);
+        assertTrue(users.size() >= 2);
         assertNotNull(dao.getUserById(user1.getId()));
         assertNotNull(dao.getUserById(user2.getId()));
     }
@@ -142,7 +140,7 @@ public class UserDAOImplTest {
     @Test
     @Transactional
     @Rollback
-    public void deleteUser_withCorrectParameter(){
+    public void deleteUser_withCorrectParameter() {
         User user = dao.createUser("testFirstName", "testLastName", "test.email@email.com", "password", UserTypes.REGULAR_USER);
 
         dvProject.setAuthor(user.getId());
@@ -157,4 +155,131 @@ public class UserDAOImplTest {
         assertNull(dao.getUserById(user.getId()));
         assertTrue(dao.getAllUserProjects(user, 4, "desc").equals(Collections.<Project>emptyList()));
     }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void giveUserAccessToProjectWithCorrectParameters() {
+        User user1 = dao.createUser("testFirstName1", "testLastName1", "test.email@email.com1", "testpassword1", UserTypes.REGULAR_USER);
+        User user2 = dao.createUser("testFirstName2", "testLastName2", "test.email@email.com2", "testpassword2", UserTypes.REGULAR_USER);
+        dvProject.setAuthor(user1.getId());
+        dvProject.setAuthorFullName(user1.getFullName());
+        dvProject = dvDao.saveProject(dvProject);
+        assertTrue(dao.giveUserAccessToProject(user2, dvProject));
+        assertTrue(dao.getAllSharedToUserProjects(user2, 4, "desc").size() == 1);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void giveUserAccessToProjectWithNullParameters() {
+        assertFalse(dao.giveUserAccessToProject(null, null));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void removeAccessToProjectFromUserWithCorrectParameters() {
+        User user1 = dao.createUser("testFirstName1", "testLastName1", "test.email@email.com1", "testpassword1", UserTypes.REGULAR_USER);
+        User user2 = dao.createUser("testFirstName2", "testLastName2", "test.email@email.com2", "testpassword2", UserTypes.REGULAR_USER);
+        dvProject.setAuthor(user1.getId());
+        dvProject.setAuthorFullName(user1.getFullName());
+        dvProject = dvDao.saveProject(dvProject);
+        dao.giveUserAccessToProject(user2, dvProject);
+        assertTrue(dao.getAllSharedToUserProjects(user2, 4, "desc").size() == 1);
+        dao.removeAccessToProjectFromUser(user2, dvProject);
+        assertTrue(dao.getAllSharedToUserProjects(user2, 4, "desc").isEmpty());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void removeAccessToProjectFromUserWithNullParameters() {
+        assertFalse(dao.removeAccessToProjectFromUser(null, null));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void createUserWithCorrectParameter() {
+        User userExpected = dao.createUser("testFirstName", "testLastName", "test.email@email.com", "testpassword", UserTypes.REGULAR_USER);
+        User userActual = dao.getUserById(userExpected.getId());
+        assertEquals(userExpected.getFirstName(), userActual.getFirstName());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void createUserWithEmailWhichAlreadyExists() {
+        User userExpected = dao.createUser("testFirstName", "testLastName", "test.email@email.com", "testpassword", UserTypes.REGULAR_USER);
+        User userActual = dao.createUser(userExpected.getFirstName(), userExpected.getLastName(), userExpected.getEmail(), userExpected.getPassword(), userExpected.getUserType());
+        assertNull(userActual);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void getUsersThatHaveAccessToProjectWitCorrectParameter() {
+        User user1 = dao.createUser("testFirstName1", "testLastName1", "test.email@email.com1", "testpassword1", UserTypes.REGULAR_USER);
+        User user2 = dao.createUser("testFirstName2", "testLastName2", "test.email@email.com2", "testpassword2", UserTypes.REGULAR_USER);
+        dvProject.setAuthor(user1.getId());
+        dvProject.setAuthorFullName(user1.getFullName());
+        dvProject = dvDao.saveProject(dvProject);
+        dao.giveUserAccessToProject(user2, dvProject);
+
+        List<User> usersThatHaveAccessToProject = dao.getUsersThatHaveAccessToProject(dvProject);
+
+        assertTrue(usersThatHaveAccessToProject.size() == 1);
+        assertEquals(user2.getId(), usersThatHaveAccessToProject.get(0).getId());
+    }
+
+    @Test
+    public void getUsersThatHaveAccessToProjectWitNullParameter() {
+        assertNull(dao.getUsersThatHaveAccessToProject(null));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void getAllUserProjectsWithCorrectParameter() {
+        User user = dao.createUser("testFirstName", "testLastName", "test.email@email.com", "password", UserTypes.REGULAR_USER);
+        dvProject.setAuthor(user.getId());
+        dvProject.setAuthorFullName(user.getFullName());
+        hmProject.setAuthor(user.getId());
+        hmProject.setAuthorFullName(user.getFullName());
+        dvDao.saveProject(dvProject);
+        hmDao.saveProject(hmProject);
+        List<Project> allUserProjects = dao.getAllUserProjects(user, 4, "desc");
+
+        assertTrue(allUserProjects.size() == 2);
+    }
+
+    @Test
+    public void getAllUserProjectsWithNullParameters() {
+        assertNull(dao.getAllUserProjects(null, 4, null));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void getAllSharedToUserProjectsWithCorrectParameter() {
+        User user1 = dao.createUser("testFirstName1", "testLastName1", "test.email@email.com1", "testpassword1", UserTypes.REGULAR_USER);
+        User user2 = dao.createUser("testFirstName2", "testLastName2", "test.email@email.com2", "testpassword2", UserTypes.REGULAR_USER);
+        dvProject.setAuthor(user1.getId());
+        dvProject.setAuthorFullName(user1.getFullName());
+        dvProject = dvDao.saveProject(dvProject);
+        dao.giveUserAccessToProject(user2, dvProject);
+
+        List<Project> allSharedToUserProjects = dao.getAllSharedToUserProjects(user2, 4, "desc");
+
+        assertTrue(allSharedToUserProjects.size() == 1);
+        assertEquals(ProjectTypes.DATA_VISUALIZATION, allSharedToUserProjects.get(0).getType());
+    }
+
+    @Test
+    public void getAllSharedToUserProjectsWithNullParameter() {
+        assertNull(dao.getAllSharedToUserProjects(null, 4, null));
+    }
+
+
 }
