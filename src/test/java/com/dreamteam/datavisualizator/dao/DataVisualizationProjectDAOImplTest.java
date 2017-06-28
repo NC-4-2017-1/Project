aa4f52b31e8f90486d6076a7dd4543316daf604d
Sergey Pavlychenko <seraphicps@mail.ru>
2017-06-28 22:37:26 +0300

    NCSPRIN-60 unit-tests for ProjectDV dev.

--- src/test/java/com/dreamteam/datavisualizator/dao/DataVisualizationProjectDAOImplTest.java
+++ src/test/java/com/dreamteam/datavisualizator/dao/DataVisualizationProjectDAOImplTest.java
@@ -1,7 +1,14 @@
 package com.dreamteam.datavisualizator.dao;
 
 import com.dreamteam.datavisualizator.common.configurations.ServletContext;
+import com.dreamteam.datavisualizator.models.Graphic;
 import com.dreamteam.datavisualizator.models.Project;
+import com.dreamteam.datavisualizator.models.User;
+import com.dreamteam.datavisualizator.models.UserTypes;
+import com.dreamteam.datavisualizator.models.impl.DataVisualizationProject;
+import com.dreamteam.datavisualizator.models.impl.GraphicDVImpl;
+import com.google.gson.JsonObject;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,9 +23,15 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -26,13 +39,12 @@ import static org.junit.Assert.assertEquals;
 public class DataVisualizationProjectDAOImplTest {
 
     @Autowired
-    private UserDAO dao;
+    private UserDAO userDAO;
+
+    private User user;
 
     @Autowired
     private DataVisualizationProjectDAO dvDao;
-
-    @Autowired
-    private HealthMonitorProjectDAO hmDao;
 
     @Configuration
     public static class DataVisualizationDaoCustomConfiguration {
@@ -51,12 +63,93 @@ public class DataVisualizationProjectDAOImplTest {
 
     private Project dvProject;
 
-    @Ignore
+    @Before
+    public void setUp() {
+        List<Graphic> graphics = new ArrayList<>();
+        Graphic dvGraphic = new GraphicDVImpl.DVGraphBuilder()
+                .buildGraphicJSON(new JsonObject())
+                .buildAverage(BigDecimal.valueOf(12.5))
+                .buildDispersion(BigDecimal.valueOf(12.5))
+                .buildOlympicAverage(BigDecimal.valueOf(12.5))
+                .buildCorrelation(null)
+                .buildMathExpectation(BigDecimal.valueOf(12.5))
+                .buildName("testDVGraph")
+                .buildGraphic();
+
+        graphics.add(dvGraphic);
+
+        dvProject = new DataVisualizationProject.Builder("DVTestProject", new Date(), null, null)
+                .buildDescription("DVTestProjectDescription")
+                .buildGraphics(graphics)
+                .buildProject();
+
+    }
+
+    public DataVisualizationProject setUpProjectDV(){
+        DataVisualizationProject project = (DataVisualizationProject) dvProject;
+        project.setAuthor(user.getId());
+        project.setAuthorFullName(user.getFullName());
+        return (DataVisualizationProject) dvDao.saveProject(project);
+    }
+
     @Test
     @Transactional
     @Rollback
-    public void getUserByIdWithCorrectId() {
-        assertEquals(new Object(), new Object());
+    public void getProjectByIdWithCorrectId(){
+        user = userDAO.createUser("testFirstName", "testLastName", "test.email@email.com", "testpassword", UserTypes.REGULAR_USER);
+        Project expectedProject = setUpProjectDV();
+        Project actualProject = dvDao.getProjectById(expectedProject.getId());
+        assertEquals(expectedProject.getId(), actualProject.getId());
+    }
+
+    @Test
+    public void getProjectByIdWithNullId(){
+        Project actual = dvDao.getProjectById(null);
+        assertNull(actual);
+    }
+
+    @Test
+    public void getProjectByIdWithIncorrectId(){
+        Project actual = dvDao.getProjectById(BigInteger.valueOf(500L));
+        assertNull(actual);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void getProjectNameWithCorrectId(){
+        user = userDAO.createUser("testFirstName", "testLastName", "test.email@email.com", "testpassword", UserTypes.REGULAR_USER);
+        DataVisualizationProject savedProject = setUpProjectDV();
+        String expected = savedProject.getName();
+        String actual = dvDao.getProjectName(savedProject.getId());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getProjectNameWithIncorrectId(){
+        String actual = dvDao.getProjectName(null);
+        assertNull(actual);
+    }
+
+
+
+    @Test
+    @Transactional
+    @Rollback
+    public void getProjectsByAuthorWithCorrectData(){
+        user = userDAO.createUser("testFirstName", "testLastName", "test.email@email.com", "testpassword", UserTypes.REGULAR_USER);
+        Project project1 = setUpProjectDV();
+        Project project2 = setUpProjectDV();
+        List<DataVisualizationProject> actualProjects = dvDao.getProjectsByAuthor(userDAO.getUserById(project1.getAuthor()));
+        assertTrue(actualProjects.size()>=2);
+        assertNotNull(dvDao.getProjectById(project1.getId()));
+        assertNotNull(dvDao.getProjectById(project2.getId()));
+    }
+
+    @Test
+    public void getProjectsByAuthorWithInCorrectData(){
+        List<DataVisualizationProject> actual = dvDao.getProjectsByAuthor(null);
+        assertNull(actual);
     }
 
 }
